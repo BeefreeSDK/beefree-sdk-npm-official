4b514bdf8670f8f1ffc654872a7c07c69efb6a19
Efrem Bonfiglio <efrem.bonfiglio@beefree.io>
2024-06-11 11:27:18 +0200

    fix: added comments to IEntityContentJson interface

--- src/types/bee.ts
+++ src/types/bee.ts
@@ -1339,6 +1339,9 @@ export interface IEntityContentJson {
     }
     title: string
   }
+  comments: {
+    [commentId: string]: BeePluginCommentPayload
+  }
 }
 
 export type BeePluginMessageEditDetailPatch = {
