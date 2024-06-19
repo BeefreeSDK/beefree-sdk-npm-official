5ea5fa62913fbd92f870c64cc15732df99770d50
Efrem Bonfiglio <efrem.bonfiglio@beefree.io>
2024-06-19 11:38:07 +0200

    fix: updated types

--- src/types/bee.ts
+++ src/types/bee.ts
@@ -861,6 +861,7 @@ export interface IInvitedMention {
   username: string
   value: string
   uid: string | number
+  userColor: string
 }
 
 export interface IPluginForm {
@@ -2117,6 +2118,12 @@ export interface IBeeConfig {
   onComment?: (commentPayload: BeePluginOnCommentPayload, json: string) => void
   onInfo?: (info: BeePluginInfo) => void
   onLoadWorkspace?: (worspaceType: LoadWorkspaceOptions) => void
+  commentingFiltersOff?: boolean
+  logLevel?: number
+  titleDefaultConfig?: {
+    bold: boolean
+  }
+  forceSanitizeHTML?: boolean
 }
 
 export interface IBeeConfigFileManager {
