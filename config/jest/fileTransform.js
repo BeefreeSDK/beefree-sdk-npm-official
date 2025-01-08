8da6154d36a1b3e44855f5e4cc1cbbc38d1e8124
Roberto Marchetti <marchetti@beefree.io>
2025-01-08 08:55:15 +0100

    fix: use ESM syntax for import in fileTransform

--- config/jest/fileTransform.js
+++ config/jest/fileTransform.js
@@ -1,9 +1,9 @@
-const path = require('path');
+import path from 'path';
 
 // This is a custom Jest transformer turning file imports into filenames.
 // http://facebook.github.io/jest/docs/tutorial-webpack.html
 
-module.exports = {
+export default {
   process(src, filename) {
     return `module.exports = ${JSON.stringify(path.basename(filename))};`;
   },
